3b9c6ac411fae1c4b88fb4fc455e2502dfe1909e
tuntematonjr <mika.lieto@hotmail.com>
2024-05-23 22:53:06 +0300

    marker name macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -8,6 +8,14 @@
     #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
 #endif
 
+//
+#define MARKER_NAME_CONFIG(var1) QUOTE(DOUBLES(ADDON,var1))
 
+#define MARKER_NAME(var1) FORMAT_2("%1_%2",QUOTE(ADDON),var1)
+
+//AAR enabled check
+#define AAR_IS_ENABLED !isnil "afi_aar2"
 //AAR update macro
-#define AAR_UPDATE(OBJ,VARNAME,VALUE) if ( !isnil "afi_aar2" ) then { [OBJ, VARNAME, VALUE] call afi_aar2_fnc_addcustomdata; };
+#define AAR_UPDATE(OBJ,VARNAME,VALUE) if ( AAR_IS_ENABLED ) then { [OBJ, VARNAME, VALUE] call afi_aar2_fnc_addcustomdata; }
+//AAR event
+#define AAR_EVENT(TEXT,INSTIGATOR,TARGET,POI) if ( AAR_IS_ENABLED ) then { if (isServer) then { [TEXT,INSTIGATOR,TARGET,POI] call afi_aar2_fnc_addCustomEvent;} else {[TEXT,INSTIGATOR,TARGET,POI] remoteExecCall ["afi_aar2_fnc_addCustomEvent", 2]} };
