a808a7d2e7e26867da9a7ff5396854ad77fc476d
tuntematonjr <mika.lieto@hotmail.com>
2024-09-05 22:08:30 +0300

    stylepoints 6/5

--- addons/main/CfgEventHandlers.hpp
+++ addons/main/CfgEventHandlers.hpp
@@ -4,7 +4,6 @@ class Extended_PreInit_EventHandlers {
     };
 };
 
-
 class Extended_PostInit_EventHandlers {
     class ADDON {
         clientInit = QUOTE(call COMPILE_FILE(XEH_postInit_client));
