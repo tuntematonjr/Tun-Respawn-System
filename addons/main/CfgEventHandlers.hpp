5919978894d380961553fe429a4a9bec09eea305
tuntematonjr <mika.lieto@hotmail.com>
2024-09-24 16:34:54 +0300

    move events to postInit

--- addons/main/CfgEventHandlers.hpp
+++ addons/main/CfgEventHandlers.hpp
@@ -1,11 +1,6 @@
-class Extended_PreInit_EventHandlers {
-    class ADDON {
-        serverInit = QUOTE(call COMPILE_FILE(XEH_preInit_server));
-    };
-};
-
 class Extended_PostInit_EventHandlers {
     class ADDON {
+        serverInit = QUOTE(call COMPILE_FILE(XEH_postInit_server));
         clientInit = QUOTE(call COMPILE_FILE(XEH_postInit_client));
     };
 };
